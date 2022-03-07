1579f188c06af7086d6731d54a0e63551b02a389
Ryan Wheeler <ryan.wheeler12@gmail.com>
2022-03-07 07:03:21 -1000

    Server

--- server/database.sql
+++ server/database.sql
@@ -1,17 +1,17 @@
 
-DROP DATABASE IF EXISTS SDCreviews;
+DROP DATABASE IF EXISTS sdcreviews;
 
-CREATE DATABASE SDCreviews;
+CREATE DATABASE sdcreviews;
 
-\c SDCreviews;
+\c sdcreviews;
 
 DROP TABLE IF EXISTS reviews, reviews_photos, characteristic_reviews;
 
 CREATE TABLE reviews(
-  id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  id INTEGER NOT NULL PRIMARY KEY,
   product_id INTEGER NOT NULL,
   rating INTEGER NULL DEFAULT NULL,
-  date TIMESTAMP NOT NULL,
+  date VARCHAR NOT NULL,
   summary VARCHAR NOT NULL,
   body VARCHAR NULL DEFAULT NULL,
   recommend BOOLEAN,
@@ -19,25 +19,37 @@ CREATE TABLE reviews(
   reviewer_name VARCHAR NOT NULL,
   reviewer_email VARCHAR,
   response VARCHAR NULL DEFAULT NULL,
-  helpfulness INTEGER NOT NULL DEFAULT 0,
+  helpfulness INTEGER NOT NULL DEFAULT 0
 );
 
 CREATE TABLE reviews_photos(
-  id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  id INTEGER NOT NULL PRIMARY KEY,
   review_id INTEGER NOT NULL,
-  url VARCHAR,
+  url VARCHAR
 );
 
 CREATE TABLE characteristic_reviews(
-  id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  id INTEGER NOT NULL PRIMARY KEY,
   characteristic_id INTEGER NOT NULL,
   review_id INTEGER NOT NULL,
-  value INTEGER NOT NULL,
+  value INTEGER NOT NULL
 );
 
--- probably need foreign keys, just dont know which ones yet
--- ALTER TABLE reviews ADD FOREIGN KEY (id_photos) REFERENCES reviews_photos (id);
+--  Not using for now
+ALTER TABLE reviews ADD FOREIGN KEY (id_photos) REFERENCES reviews_photos (id);
+ALTER TABLE reviews ADD FOREIGN KEY (id_characteristics) REFERENCES characteristic_reviews (id);
 
-\copy reviews FROM '../reviews.csv' DELIMITER ',' null as 'null' CSV header;
-\copy reviews_photos FROM '../reviews_photos.csv' DELIMITER ',' CSV header;
-\copy characteristic_reviews FROM '../characteristic_reviews.csv' DELIMITER ',' CSV header;
+\copy reviews FROM '/Users/rizztown/Desktop/SDC/reviews.csv' DELIMITER ',' null as 'null' CSV header;
+\copy reviews_photos FROM '/Users/rizztown/Desktop/SDC/reviews_photos.csv' DELIMITER ',' CSV header;
+\copy characteristic_reviews FROM '/Users/rizztown/Desktop/SDC/characteristic_reviews.csv' DELIMITER ',' CSV header;
+
+-- Building
+
+SELECT
+  body,
+  date,
+  helpfulness,
+  array_agg(reviews_photos.url) AS photos
+FROM
+  reviews
+JOIN reviews_photos ON (reviews.id = reviews_photos.review_id)
--- server/index.js
+++ server/index.js
@@ -12,27 +12,71 @@ app.use(express.json());
 //create review
 app.post("/reviews", async(req, res) => {
   try {
-    const { product_id, name, body, rating, summary, recommend, photos, characteristics } = req.body;
-    const newReview = await pool.query(
-      "INSERT INTO reviews (product_id, name, body, rating, summary, recommend, photos, characteristics) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *",
-      [product_id, name, body, rating, summary, recommend, photos, characteristics]
-    );
-    res.json(newReview.rows[0]);
+    console.log('IN post; req.body: ', req.body);
+    // const { product_id, name, body, rating, summary, recommend, photos, characteristics } = req.body;
+    // const newReview = await pool.query(
+    //   "INSERT INTO reviews (product_id, name, body, rating, summary, recommend, photos, characteristics) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *",
+    //   [product_id, name, body, rating, summary, recommend, photos, characteristics]
+    // );
+    // res.json(newReview.rows[0]);
   } catch (err) {
     console.error(err.message);
   }
 });
 
 //get reviews  --NOT DONE YET--
+
 app.get("/reviews", async(req, res) => {
   try {
-    const { count, sort, product_id } = req.body;
-    const getReviews = await pool.query("SELECT  FROM reviews");
-    res.json(getReviews.rows);
+    console.log('In get reviews;req: ', req.query);
+    // console.log('In get reviews;req.params: ', req.params);
+    // console.log('In get reviews;req.body: ', req.body);
+    const { count, sort, product_id } = req.query;
+    const resultObj = {};
+    const infoStuff = await pool.query(`
+    SELECT
+      body,
+      date,
+      helpfulness,
+      rating,
+      recommend,
+      response,
+      reviews.id AS review_id,
+      reviewer_name,
+      summary,
+      p.photos
+      FROM (
+        SELECT
+        array_agg(reviews_photos.url) AS photos,
+        review_id
+        FROM
+        reviews_photos
+        GROUP BY review_id
+      ) p JOIN reviews ON reviews.id = p.review_id
+    WHERE reviews.product_id = $1
+    `, [product_id]);
+    console.log(infoStuff.rows);
+    // console.log('Get query limit 5 from reviews: ');
+    // const obj = {results: [{body: "bla bla", date: "2022-02-22T00:00:00.000Z", helpfulness: 63, photos: [], rating: 5, recommend: false, response: null, review_id: 1135857, reviewer_name: "poop", summary: "this bla"}, {body: "second bla", date: "2022-02-22T00:00:00.000Z", helpfulness: 53, photos: [], rating: 3, recommend: false, response: null, review_id: 1134857, reviewer_name: "pop", summary: "this still bla"}]};
+    res.json(infoStuff.rows);
   } catch (err) {
     console.error(err.message)
   }
 })
+
+// app.get("/reviews", async(req, res) => {
+//   try {
+//     const { count, sort, product_id } = req.params;
+
+//     // const getReviews = await pool.query("SELECT json_build_object('product', $1, 'page', 0, 'count', $2, 'results', (SELECT json_agg()) ");
+
+//     console.log(req.body);
+//     const getReviews = await pool.query("SELECT * FROM reviews LIMIT 10");
+//     res.json(getReviews);
+//   } catch (err) {
+//     console.error(err.message)
+//   }
+// })
 
 //put reviews (helpful, reported)
 
